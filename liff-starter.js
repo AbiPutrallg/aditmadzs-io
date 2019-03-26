80d449c185238a7a6d27dd67aaf9fe1c668246f4
Adit <32681526+Aditmadzs@users.noreply.github.com>
2019-03-26 22:43:25 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,83 +1,62 @@
-
 window.onload = function (e) {
     liff.init(function () {
-        initializeApp();
-    });
-    $('button').hover(function(){
-    $('.glitch-wrapper').toggleClass('paused');
-    $('body').toggleClass('paused');
+        getP();
     });
 };
 
-function initializeApp(){
-    var type = getParameterByName('type')
-    if (type=== "text") {
-        liff.sendMessages([{type: 'text',text: getParameterByName('text')}]).then(function () {liff.closeWindow()});
-    }else if(type=="sticker"){
-        var stk = getParameterByName('tstk');
-        var sid = getParameterByName('stkid');
-        var pkg = getParameterByName('stkpkgid');
-        var send = getParameterByName('send');
-        var uriz="line://shop/sticker/detail/"+pkg;
-        var tp="";
-        var ep = '';
-        if (stk === 'animasi') {
-            ep = "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+"/IOS/sticker_animation@2x.png";
-            tp="a";
-        } else {
-            ep = "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+"/IOS/sticker@2x.png";
-            tp="n";
-        }
-        if (send===null){
-        }else{
-            if(send===true || send==="true")
-                if(tp==="a"){
-                    uriz="line://app/1600328768-y3yq64nw/?type=sticker&tstk=animasi&stkid="+sid+"&stkpkgid="+pkg+"&send=true";
-                }else{
-                    uriz="line://app/1600328768-y3yq64nw/?type=sticker&tstk=anime&stkid="+sid+"&stkpkgid="+pkg+"&send=true";
-                }
-        }
-        liff.sendMessages([{
-          type: "template",
-          altText: "Sticker",
-          template: {
-             type: "image_carousel",
-             columns: [{
-                 imageUrl: ep,
-                 action: {
-                     type: "uri",
-                     uri: uriz}}
-                          ]
-                        }
-        }]).then(function () {
-            liff.closeWindow();
+function getP(){
+    var tipe = getParameterByName('type')
+    if (!tipe) {
+        document.getElementById('textx').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: atob('bGluZTovL2FwcC8xNjAyNjg3MzA4LUdYcTRWdms5P3R5cGU9dGV4dCZ0ZXh0PVlvdXIlMjBUZXh0Cgp0eXBlID0+IHRleHQKdGV4dCA9PiB5b3VyIHRleHQ=')
+            }]).then(function () {
+                liff.closeWindow();
+            });
         });
-    }else if (type === 'audio') {
-        liff.sendMessages([{
-            type: 'audio',
-            originalContentUrl: getParameterByName('link'),
-            duration: 60000
-        }]).then(function () {
-            liff.closeWindow();
+        document.getElementById('imagex').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: atob('bGluZTovL2FwcC8xNjAyNjg3MzA4LUdYcTRWdms5P3R5cGU9aW1hZ2UmaW1nPWh0dHBzOi8vd2FsbHBhcGVyc3R1ZGlvMTAuY29tL3N0YXRpYy93cGRiL3dhbGxwYXBlcnMvMTAwMHg1NjMvMTY4ODkxLmpwZwoKdHlwZSA9PiBpbWFnZQppbWcgPT4gTGluayAobXVzdCBiZSBIVFRQUyk=')
+            }]).then(function () {
+                liff.closeWindow();
+            });
         });
-    }else if (type === 'image') {
-        liff.sendMessages([{
-            type: 'image',
-            originalContentUrl: getParameterByName('img'),
-            previewImageUrl: getParameterByName('img')
-        }]).then(function () {
-            liff.closeWindow();
+        document.getElementById('videox').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: atob('bGluZTovL2FwcC8xNjAyNjg3MzA4LUdYcTRWdms5P3R5cGU9dmlkZW8mb2N1PWh0dHBzOi8vdGlueXVybC5jb20veThvZzNvcjUmcGl1PWh0dHBzOi8vaW1hZ2VzNi5hbHBoYWNvZGVycy5jb20vNzEwL3RodW1iLTM1MC03MTAxMzIucG5nCgp0eXBlID0+IHZpZGVvCm9jdSA9PiB2aWRlbyB1cmwKcGl1ID0+IHByZXZpZXcgaW1hZ2U=')
+            }]).then(function () {
+                liff.closeWindow();
+            });
         });
-    }else if (type === 'video') {
-        liff.sendMessages([{
-            type: 'video',
-            originalContentUrl: getParameterByName('ocu'),
-            previewImageUrl: getParameterByName('piu')
-        }]).then(function () {
-            liff.closeWindow();
+        document.getElementById('audiox').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: atob('bGluZTovL2FwcC8xNjAyNjg3MzA4LUdYcTRWdms5P3R5cGU9YXVkaW8mbGluaz1odHRwczovL3BsYXRlbGV0cy5mdW4vcHVibGljL3NvdW5kcy9tdXNpYy5tcDM=')
+            }]).then(function () {
+                liff.closeWindow();
+            });
         });
+        document.getElementById('stickerx').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: atob('V2l0aCBBbmltYXRpb246CmxpbmU6Ly9hcHAvMTYwMjY4NzMwOC1HWHE0VnZrOT90eXBlPXN0aWNrZXImc3RrPWFuaW0mc2lkPTMyMTI4MjMxJnBrZz0zMDk5MzEyCgpObyBBbmltYXRpb246CmxpbmU6Ly9hcHAvMTYwMjY4NzMwOC1HWHE0VnZrOT90eXBlPXN0aWNrZXImc3RrPW5vYW5pbSZzaWQ9MzIxMjgyMzEmcGtnPTMwOTkzMTIKCnR5cGUgPT4gc3RpY2tlcgpzdGsgPT4gYW5pbSAvIG5vYW5pbQpzaWQgPT4gc3RpY2tlciBpZApwa2cgPT4gcGFja2FnZXMgaWQ=')
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+    } else {
+        makeText();
+        makeImage();
+        makeVideo();
+        makeAudio();
+        makeSticker();
+        meProfile();
     }
-}
+    }
+
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
@@ -87,17 +66,108 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
+function getProfile(){
+    liff.getProfile().then(function (profile) {
+        document.getElementById('userid').textContent = 'Hai  ' + profile.displayName;
+        document.getElementById('main').src = profile.pictureUrl;        
+        document.getElementById('close').addEventListener('click', function () {
+            liff.closeWindow();
+        });
+    });
+}
+
+function makeText(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'text') {
+        liff.sendMessages([{
+            type: 'text',
+            text: getParameterByName('text')
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeImage(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'image') {
+        liff.sendMessages([{
+            type: 'image',
+            originalContentUrl: getParameterByName('img'),
+            previewImageUrl: getParameterByName('img')
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeVideo(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'video') {
+        liff.sendMessages([{
+            type: 'video',
+            originalContentUrl: getParameterByName('ocu'),
+            previewImageUrl: getParameterByName('piu')
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeAudio(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'audio') {
+        liff.sendMessages([{
+            type: 'audio',
+            originalContentUrl: getParameterByName('link'),
+            duration: 60000
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeSticker(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'sticker') {
+        var stk = getParameterByName('stk');
+        var sid = getParameterByName('sid');
+        var pkg = getParameterByName('pkg');
+        var ep = '';
+        if (stk === 'anim') {
+            ep = "/IOS/sticker_animation@2x.png";
+        } else {
+            ep = "/IOS/sticker@2x.png";
+        }
+        liff.sendMessages([{
+          type: "template",
+          altText: "Sticker",
+          template: {
+             type: "image_carousel",
+             columns: [{
+                 imageUrl: "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+ep,
+                 action: {
+                     type: "uri",
+                     uri: "line://shop/sticker/detail/"+pkg}}
+                          ]
+                        }
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
 function meProfile(){
-    var type = getParameterByName('type');
+    var tipe = getParameterByName('type');
     liff.getProfile().then(function (prof) {
         var stat = prof.statusMessage;
         if (stat == null) {
             var stat = " - ";
         }
-        if (stat.length > 100) {
-            var stat = "MAX STATUS MESSAGE ADITMADZS 100 WORDS";
+        if (stat.length > 60) {
+            var stat = "Status Message is to long! Max 60 words";
         }
-        if (type === 'profile') {
+        if (tipe === 'profile') {
             liff.sendMessages([{
                 type: "template",
                 altText: "Profile "+prof.displayName,
